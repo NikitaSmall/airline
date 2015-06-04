468c8c33775277d426c9234a873ed67127865f3a
nikitasmall <nikitasosnov@yahoo.com>
2015-06-04 12:10:39 +0300

    jade template engine implemented

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var http = require('http');
 var fs = require('fs');
 var path = require('path');
 var mime = require('mime');
+var jade = require('jade');
 
 var cache = {};
 
@@ -12,6 +13,12 @@ function send404(res) {
 }
 
 function sendFile(res, filePath, fileContent) {
+  if (path.extname(filePath) === '.jade') {
+    var fn = jade.compileFile(filePath);
+    var html = fn();
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(html);
+  }
   res.writeHead(
     200,
     { "Content-Type": mime.lookup(path.basename(filePath)) }
@@ -31,14 +38,21 @@ function serveFile(err, data) {
 function setFileToCache(res, cache, absPath) {
   fs.exists(absPath, function(exists) {
     if (exists) {
-      fs.readFile(absPath, serveFile);
+      fs.readFile(absPath, function(err, data) {
+        if (err) {
+          send404(res); // not found
+        } else {
+          cache[absPath] = data; // save in cache
+          sendFile(res, absPath, cache[absPath]); // serve cached file
+        }
+      });
     } else {
       send404(res); // not found
     }
   });
 };
 
-//TODO: check the refactored hell
+//TODO: check this refactored hell
 // serve static files, but first - let them to cache
 function serveStatic(res, cache, absPath) {
   if (cache[absPath]) {
@@ -51,7 +65,7 @@ function serveStatic(res, cache, absPath) {
 var server = http.createServer(function(req, res) {
   var filePath = false;
   if (req.url == '/') {
-    filePath = 'public/index.html';
+    filePath = 'public/index.jade';
   } else {
     filePath = 'public' + req.url;
   }
